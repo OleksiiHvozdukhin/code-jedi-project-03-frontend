0a3cb12812a6410473aea8e97ab717b29d15de3d
Hennadii Laktionov <morifer13@gmail.com>
2023-12-21 03:28:04 +0200

    some setup
    add global style, theme and sprite

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { theme } from 'components/theme';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
