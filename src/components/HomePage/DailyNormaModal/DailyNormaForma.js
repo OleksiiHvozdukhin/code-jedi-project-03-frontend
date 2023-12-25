67d46fe8bb1412be8ec1b14703bf34809a453094
Yuliia Chaika <djuliya72@ukr.net>
2023-12-25 17:59:45 +0200

    added styles

--- src/components/HomePage/DailyNormaModal/DailyNormaForma.js
+++ src/components/HomePage/DailyNormaModal/DailyNormaForma.js
@@ -1,12 +1,22 @@
 import * as Yup from 'yup';
-import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { Field, Formik } from 'formik';
+import {
+  BtnSave,
+  Input,
+  Label,
+  LabelRadio,
+  RadioGroup,
+  Span,
+  StyledErrorMessage,
+  StyledForm,
+} from './DailyNormaForma.styled';
 
 export const DailyNormaForma = () => {
   const formValidationSchema = Yup.object().shape({
     gender: Yup.string().required('Please select a gender'),
     weight: Yup.number()
       .required('Enter weight')
-      .min(0, 'Weight must be greater than 0'),
+      .min(10, 'Weight must be greater than 0'),
     activityTime: Yup.number()
       .required('Enter activity time')
       .min(0, 'Activity time must be greater than 0'),
@@ -20,10 +30,11 @@ export const DailyNormaForma = () => {
     // saveDailyNorma(values);
     resetForm();
   };
+
   return (
     <Formik
       initialValues={{
-        gender: '',
+        gender: 'woman',
         weight: 0,
         activityTime: 0,
         dailyNorma: 0,
@@ -31,32 +42,58 @@ export const DailyNormaForma = () => {
       validationSchema={formValidationSchema}
       onSubmit={handleSubmit}
     >
-      <Form>
-        <label htmlFor="woman">For woman</label>
-        <Field type="radio" id="woman" name="gender" value="woman" />
-        <label htmlFor="man">For man</label>
-        <Field type="radio" id="man" name="gender" value="man" />
-        <ErrorMessage name="gender" component="div" />
-        <label htmlFor="weight">Your weight in kilograms:</label>
-        <Field type="number" name="weight"></Field>
-        <ErrorMessage name="weight" component="div" />
-        <label htmlFor="activityTime">
-          The time of active participation in sports or other activities with a
-          high physical. load:
-        </label>
-        <Field type="number" name="activityTime"></Field>
-        <ErrorMessage name="activityTime" component="div" />
-        <p>
-          The required amount of water in liters per day: <span>1.8 L</span>
-        </p>
-        <label htmlFor="dailyNorma">
-          The time of active participation in sports or other activities with a
-          high physical. load:
-        </label>
-        <Field type="number" name="dailyNorma"></Field>
-        <ErrorMessage name="dailyNorma" component="div" />
-        <button type="submit">Save</button>
-      </Form>
+      {({ errors, touched }) => (
+        <StyledForm>
+          <RadioGroup>
+            <Input
+              type="radio"
+              id="woman"
+              name="gender"
+              value="woman"
+              checked
+              className={errors.gender && touched.gender ? 'error' : ''}
+            />
+            <LabelRadio htmlFor="woman">For woman</LabelRadio>
+            <Field type="radio" id="man" name="gender" value="man" />
+            <LabelRadio htmlFor="man">For man</LabelRadio>
+          </RadioGroup>
+          <StyledErrorMessage name="gender" component="div" />
+          <Label htmlFor="weight">Your weight in kilograms:</Label>
+          <Input
+            type="number"
+            name="weight"
+            className={`special ${
+              errors.weight && touched.weight ? 'error' : ''
+            }`}
+          />
+          <StyledErrorMessage name="weight" component="div" />
+          <Label htmlFor="activityTime">
+            The time of active participation in sports or other activities with
+            a high physical. load:
+          </Label>
+          <Input
+            type="number"
+            name="activityTime"
+            className={`special ${
+              errors.activityTime && touched.activityTime ? 'error' : ''
+            }`}
+          />
+          <StyledErrorMessage name="activityTime" component="div" />
+          <Label className="special">
+            The required amount of water in liters per day: <Span>1.8 L</Span>
+          </Label>
+          <Label htmlFor="dailyNorma" className="special last">
+            Write down how much water you will drink:
+          </Label>
+          <Input
+            type="number"
+            name="dailyNorma"
+            className={errors.dailyNorma && touched.dailyNorma ? 'error' : ''}
+          />
+          <StyledErrorMessage name="dailyNorma" component="div" />
+          <BtnSave type="submit">Save</BtnSave>
+        </StyledForm>
+      )}
     </Formik>
   );
 };
--- src/components/HomePage/DailyNormaModal/DailyNormaModal.js
+++ src/components/HomePage/DailyNormaModal/DailyNormaModal.js
@@ -1,6 +1,17 @@
 import Modal from 'react-modal';
-// import '../../../images/sprite.svg';
 import { DailyNormaForma } from './DailyNormaForma';
+import {
+  BtnClose,
+  CalculateTitle,
+  ModalContainer,
+  SmallText,
+  SmallTextWrapper,
+  Span,
+  Text,
+  TextWrapper,
+  Title,
+  TitleWrapper,
+} from './DailyNormaModal.styled';
 
 const customStyles = {
   content: {
@@ -10,8 +21,6 @@ const customStyles = {
     bottom: 'auto',
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
-    width: '592px',
-    height: '712px',
     zIndex: '1300',
     padding: 0,
   },
@@ -21,33 +30,57 @@ const customStyles = {
 };
 Modal.setAppElement('#root');
 
+// export const CloseIcon = () => {
+//   return (
+//     <svg>
+//       <use href="../../../images/sprite.svg#icon-close"></use>
+//     </svg>
+//   );
+// };
+
 export const DailyNormaModal = ({ isOpen, onRequestClose }) => {
   return (
     <Modal isOpen={isOpen} onRequestClose={onRequestClose} style={customStyles}>
-      <div>
-        <button onClick={onRequestClose}>
-          <svg>
-            <use src="../../../images/sprite.svg#icon-plus-small" />
-          </svg>
-        </button>
-        <h2>My daily norma</h2>
-        <div>
-          <p>
-            For woman:<span>V=(M*0,03) + (T*0,4)</span>
-          </p>
-          <p>
-            For man:<span>V=(M*0,04) + (T*0,6)</span>
-          </p>
-        </div>
-        <p>
-          <span>*</span>V is the volume of the water norm in liters per day, M
-          is your body weight, T is the time of active sports, or another type
-          of activity commensurate in terms of loads (in the absence of these,
-          you must set 0)
-        </p>
-        <h3>Calculate your rate:</h3>
+      <ModalContainer>
+        <TitleWrapper>
+          <Title>My daily norma</Title>
+          <BtnClose onClick={onRequestClose}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+            >
+              <path
+                d="M6 18L18 6M6 6L18 18"
+                stroke="#407BFF"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            {/* <StyledSvgClose /> */}
+          </BtnClose>
+        </TitleWrapper>
+
+        <TextWrapper>
+          <Text>
+            For woman: <Span>V=(M*0,03) + (T*0,4)</Span>
+          </Text>
+          <Text>
+            For man: <Span>V=(M*0,04) + (T*0,6)</Span>
+          </Text>
+        </TextWrapper>
+        <SmallTextWrapper>
+          <SmallText>
+            <span>*</span>&nbsp;V is the volume of the water norm in liters per
+            day, M is your body weight, T is the time of active sports, or
+            another type of activity commensurate in terms of loads (in the
+            absence of these, you must set 0)
+          </SmallText>
+        </SmallTextWrapper>
+        <CalculateTitle>Calculate your rate:</CalculateTitle>
         <DailyNormaForma />
-      </div>
+      </ModalContainer>
     </Modal>
   );
 };
