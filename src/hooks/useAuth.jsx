50797af18ffb9d0f576eb792ad1c301a7f76e51e
NataDent <cherkashyna.n@gmail.com>
2023-12-26 17:13:54 +0200

    Changed useAuth.js

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -4,11 +4,7 @@ import {
   selectUser,
   selectIsLoggedIn,
   selectIsRefreshing,
-<<<<<<< HEAD
 } from 'redux/auth/authSelectors.js';
-=======
-} from 'redux/auth/authSelectors';
->>>>>>> a4849c11bac75bfb3f61b82021a1986770d3fcc1
 
 export const useAuth = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
