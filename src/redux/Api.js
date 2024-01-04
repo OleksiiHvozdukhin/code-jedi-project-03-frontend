363bd1ede65a4f091822a7f5b31493f5943920d6
Alexey Hvozduhin <124602564+OleksiiHvozdukhin@users.noreply.github.com>
2024-01-04 21:56:28 +0200

    Add auth

--- src/redux/Api.js
+++ src/redux/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // !Вставить сюда ссылку на онрендер
-axios.defaults.baseURL = '';
+axios.defaults.baseURL = 'http://localhost:8000';
 
 const token = {
   set(token) {
@@ -29,16 +29,19 @@ export const logout = async () => {
   token.unset();
 };
 
+///////////////////////////////////////////////////////////////////////////////////////////
+
+//! ???
 export const refreshUser = async token => {
   token.set(token);
-  const { data } = await axios.get('/user/current');
+  const { data } = await axios.get('/users/current');
   return data;
 };
 
-export const updateAvatar = async newPhotoFile => {
+export const updateAvatar = async newPhoto => {
   const {
     data: { avatarURL },
-  } = await axios.post('/user/avatar', newPhotoFile, {
+  } = await axios.post('/users/avatar', newPhoto, {
     headers: {
       'Content-Type': 'multipart/form-data',
     },
@@ -46,11 +49,57 @@ export const updateAvatar = async newPhotoFile => {
   return avatarURL;
 };
 
+export const updateWaterNorm = async newWaterRate => {
+  const { data } = await axios.patch('users/waterRate', {
+    waterRate: newWaterRate,
+  });
+  return data;
+};
+
 export const editUserInfo = async body => {
-  const data = await axios.patch('/user/edit', body, {
+  const data = await axios.patch('/users', body, {
     headers: {
       'Content-Type': 'application/json',
     },
   });
+  return data;
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// consumedWater
+
+export const addWaters = async newWater => {
+  const { data } = await axios.post('/consumedWater/today', newWater, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return data;
+};
+
+export const editWater = async ({ newWaterUser, id }) => {
+  const { data } = await axios.patch(
+    `/consumedWater/today/${id}`,
+    newWaterUser,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+  return data;
+};
+
+export const deleteWater = async id => {
+  await axios.delete(`/consumedWater/today/${id}`);
+};
+
+export const getAllConsumedWaterMonth = async month => {
+  const { data } = await axios.get(`/consumedWater/month/${month}`);
+  return data;
+};
+
+export const getAllConsumedWaterToday = async (date, month) => {
+  const { data } = await axios.get(`/consumedWater/today/${date}/${month}`);
   return data;
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import {
-  persistStore,
-  persistReducer,
   FLUSH,
-  REHYDRATE,
-  PAUSE,
   PERSIST,
   PURGE,
   REGISTER,
+  REHYDRATE,
+  PAUSE,
 } from 'redux-persist';
+import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { authReducer } from './auth/authSlice';
 import { waterRateReducer } from './waterRate/waterRateSlice';
@@ -19,6 +18,15 @@ const authPersistConfig = {
   whitelist: ['token'],
 };
 
+const ignoredPersistenceActions = [
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+];
+
 export const store = configureStore({
   reducer: {
     waterRate: waterRateReducer,
@@ -27,7 +35,7 @@ export const store = configureStore({
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        ignoredActions: ignoredPersistenceActions,
       },
     }),
   devTools: process.env.NODE_ENV === 'development',
