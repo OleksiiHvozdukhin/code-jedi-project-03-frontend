a96516d2c9db9fe68a28185aaa8865085ef24b59
NataDent <cherkashyna.n@gmail.com>
2024-01-05 18:21:57 +0200

    SettigForma has merged to SettingModal

--- src/components/Header/SettingModal/SettingForma/SettingForma.js
+++ src/components/Header/SettingModal/SettingForma/SettingForma.js
@@ -1,211 +1,184 @@
-import { validateUserInfoSchema } from 'schemas/validateUserInfoSchema.js';
-import { Formik } from 'formik';
-import {
-  LabelRadio,
-  RadioGroup,
-  StyledForm,
-  Input,
-  FieldWrapper,
-  PasswordLabel,
-  UserLabel,
-  SubmitBtn,
-  PasswordBox,
-  PasswordBoxTitle,
-  ErrorMessage,
-  EyeBtn,
-  IconEye,
-  InputWrapper,
-  RadioBox,
-} from './SettingForma.styled';
-import sprite from '../../../../images/sprite.svg';
-// import { editUserInfoThunk } from 'redux/auth/authOperations';
-// import { useDispatch } from 'react-redux';
+// import { Formik } from 'formik';
+// import {
+//   LabelRadio,
+//   RadioGroup,
+//   StyledForm,
+//   Input,
+//   FieldWrapper,
+//   PasswordLabel,
+//   UserLabel,
+//   SubmitBtn,
+//   PasswordBox,
+//   PasswordBoxTitle,
+//   ErrorMessage,
+//   EyeBtn,
+//   IconEye,
+//   InputWrapper,
+//   RadioBox,
+// } from './SettingForma.styled';
+// import sprite from '../../../../images/sprite.svg';
+// // import { editUserInfoThunk } from 'redux/auth/authOperations';
+// // import { useDispatch } from 'react-redux';
 
-import { useState } from 'react';
+// import { useState } from 'react';
 
-export const SettingForma = ({ onRequestClose }) => {
-  // const { name, email, gender } = useAuth();
+// export const SettingForma = ({ onRequestClose }) => {
+// const { name, email, gender } = useAuth();
 
-  const [isPassword, setIsPassword] = useState({
-    oldPassword: false,
-    newPassword: false,
-    confirmPassword: false,
-  });
+// const dispatch = useDispatch();
 
-  const initialValues = {
-    name: '',
-    email: '',
-    gender: 'woman',
-    oldPassword: '',
-    newPassword: '',
-    confirmPassword: '',
-  };
+// try {
+//   await dispatch(editUserInfoThunk({ ...values, id }));
 
-  // const dispatch = useDispatch();
+// } catch (error) {
+//   console.log(error.message);
+// }
 
-  const handleTogglePassword = passwordKey => {
-    setIsPassword(prev => ({
-      ...prev,
-      [passwordKey]: !prev[passwordKey],
-    }));
-  };
+//   return (
+//     <Formik
+//       initialValues={initialValues}
+//       validationSchema={validateUserInfoSchema}
+//       onSubmit={handleSubmit}
+//     >
+//       {({ errors, touched, values, handleChange }) => (
+//         <StyledForm>
+//           <RadioGroup>
+//             <LabelRadio>Your gender identity</LabelRadio>
+//             <RadioBox>
+//               <Input
+//                 type="radio"
+//                 id="woman"
+//                 name="gender"
+//                 checked={values.gender === 'woman'}
+//                 onChange={handleChange}
+//                 className={errors.gender && touched.gender ? 'error' : ''}
+//               />
+//               <LabelRadio htmlFor="woman">Girl</LabelRadio>
+//               <Input
+//                 type="radio"
+//                 id="man"
+//                 name="gender"
+//                 checked={values.gender === 'man'}
+//                 onChange={handleChange}
+//               />
+//               <LabelRadio htmlFor="man">Man</LabelRadio>
+//             </RadioBox>
+//           </RadioGroup>
 
-  const handleSubmit = async values => {
-    console.log('data has been saved');
-    onRequestClose();
-    // try {
-    //   await dispatch(editUserInfoThunk({ ...values, id }));
+//           <FieldWrapper>
+//             <UserLabel htmlFor="name">Your name</UserLabel>
+//             <InputWrapper>
+//               <Input id="name" name="name" />
+//               <ErrorMessage component="div" name="name" />
+//             </InputWrapper>
+//           </FieldWrapper>
 
-    // } catch (error) {
-    //   console.log(error.message);
-    // }
-  };
+//           <FieldWrapper>
+//             <UserLabel htmlFor="email">Your email</UserLabel>
+//             <InputWrapper>
+//               <Input id="email" name="email" />
+//               <ErrorMessage component="div" name="email" />
+//             </InputWrapper>
+//           </FieldWrapper>
 
-  return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validateUserInfoSchema}
-      onSubmit={handleSubmit}
-    >
-      {({ errors, touched, values, handleChange }) => (
-        <StyledForm>
-          <RadioGroup>
-            <LabelRadio>Your gender identity</LabelRadio>
-            <RadioBox>
-              <Input
-                type="radio"
-                id="woman"
-                name="gender"
-                checked={values.gender === 'woman'}
-                onChange={handleChange}
-                className={errors.gender && touched.gender ? 'error' : ''}
-              />
-              <LabelRadio htmlFor="woman">Girl</LabelRadio>
-              <Input
-                type="radio"
-                id="man"
-                name="gender"
-                checked={values.gender === 'man'}
-                onChange={handleChange}
-              />
-              <LabelRadio htmlFor="man">Man</LabelRadio>
-            </RadioBox>
-          </RadioGroup>
+//           <PasswordBox>
+//             <PasswordBoxTitle>Password</PasswordBoxTitle>
 
-          <FieldWrapper>
-            <UserLabel htmlFor="name">Your name</UserLabel>
-            <InputWrapper>
-              <Input id="name" name="name" />
-              <ErrorMessage component="div" name="name" />
-            </InputWrapper>
-          </FieldWrapper>
+//             <FieldWrapper>
+//               <PasswordLabel htmlFor="oldPassword">
+//                 Outdated password:
+//               </PasswordLabel>
+//               <InputWrapper>
+//                 <Input
+//                   id="oldPassword"
+//                   name="oldPassword"
+//                   placeholder="Password"
+//                   type={isPassword.oldPassword ? 'text' : 'password'}
+//                 />
 
-          <FieldWrapper>
-            <UserLabel htmlFor="email">Your email</UserLabel>
-            <InputWrapper>
-              <Input id="email" name="email" />
-              <ErrorMessage component="div" name="email" />
-            </InputWrapper>
-          </FieldWrapper>
+//                 <EyeBtn
+//                   onClick={() => handleTogglePassword('oldPassword')}
+//                   type="button"
+//                 >
+//                   <IconEye width="16" height="16" stroke="#407BFF" fill="none">
+//                     <use
+//                       xlinkHref={
+//                         isPassword.oldPassword
+//                           ? `${sprite}#icon-eye-slash`
+//                           : `${sprite}#icon-eye`
+//                       }
+//                     />
+//                   </IconEye>
+//                 </EyeBtn>
+//                 <ErrorMessage component="div" name="oldPassword" />
+//               </InputWrapper>
+//             </FieldWrapper>
 
-          <PasswordBox>
-            <PasswordBoxTitle>Password</PasswordBoxTitle>
+//             <FieldWrapper>
+//               <PasswordLabel htmlFor="newPassword">New password:</PasswordLabel>
 
-            <FieldWrapper>
-              <PasswordLabel htmlFor="oldPassword">
-                Outdated password:
-              </PasswordLabel>
-              <InputWrapper>
-                <Input
-                  id="oldPassword"
-                  name="oldPassword"
-                  placeholder="Password"
-                  type={isPassword.oldPassword ? 'text' : 'password'}
-                />
+//               <InputWrapper>
+//                 <Input
+//                   id="newPassword"
+//                   name="newPassword"
+//                   placeholder="Password"
+//                   type={isPassword.newPassword ? 'text' : 'password'}
+//                 />
 
-                <EyeBtn
-                  onClick={() => handleTogglePassword('oldPassword')}
-                  type="button"
-                >
-                  <IconEye width="16" height="16" stroke="#407BFF" fill="none">
-                    <use
-                      xlinkHref={
-                        isPassword.oldPassword
-                          ? `${sprite}#icon-eye-slash`
-                          : `${sprite}#icon-eye`
-                      }
-                    />
-                  </IconEye>
-                </EyeBtn>
-                <ErrorMessage component="div" name="oldPassword" />
-              </InputWrapper>
-            </FieldWrapper>
+//                 <EyeBtn
+//                   onClick={() => handleTogglePassword('newPassword')}
+//                   type="button"
+//                 >
+//                   <IconEye width="16" height="16" stroke="#407BFF" fill="none">
+//                     <use
+//                       xlinkHref={
+//                         isPassword.newPassword
+//                           ? `${sprite}#icon-eye-slash`
+//                           : `${sprite}#icon-eye`
+//                       }
+//                     />
+//                   </IconEye>
+//                 </EyeBtn>
+//                 <ErrorMessage component="div" name="newPassword" />
+//               </InputWrapper>
+//             </FieldWrapper>
 
-            <FieldWrapper>
-              <PasswordLabel htmlFor="newPassword">New password:</PasswordLabel>
+//             <FieldWrapper>
+//               <PasswordLabel htmlFor="confirmPassword">
+//                 Repeat new password:
+//               </PasswordLabel>
+//               <InputWrapper>
+//                 <Input
+//                   id="confirmPassword"
+//                   name="confirmPassword"
+//                   placeholder="Password"
+//                   type={isPassword.confirmPassword ? 'text' : 'password'}
+//                 />
 
-              <InputWrapper>
-                <Input
-                  id="newPassword"
-                  name="newPassword"
-                  placeholder="Password"
-                  type={isPassword.newPassword ? 'text' : 'password'}
-                />
+//                 <EyeBtn
+//                   onClick={() => handleTogglePassword('confirmPassword')}
+//                   type="button"
+//                 >
+//                   <IconEye width="16" height="16" stroke="#407BFF" fill="none">
+//                     <use
+//                       xlinkHref={
+//                         isPassword.confirmPassword
+//                           ? `${sprite}#icon-eye-slash`
+//                           : `${sprite}#icon-eye`
+//                       }
+//                     />
+//                   </IconEye>
+//                 </EyeBtn>
+//                 <ErrorMessage component="div" name="confirmPassword" />
+//               </InputWrapper>
+//             </FieldWrapper>
+//           </PasswordBox>
 
-                <EyeBtn
-                  onClick={() => handleTogglePassword('newPassword')}
-                  type="button"
-                >
-                  <IconEye width="16" height="16" stroke="#407BFF" fill="none">
-                    <use
-                      xlinkHref={
-                        isPassword.newPassword
-                          ? `${sprite}#icon-eye-slash`
-                          : `${sprite}#icon-eye`
-                      }
-                    />
-                  </IconEye>
-                </EyeBtn>
-                <ErrorMessage component="div" name="newPassword" />
-              </InputWrapper>
-            </FieldWrapper>
-
-            <FieldWrapper>
-              <PasswordLabel htmlFor="confirmPassword">
-                Repeat new password:
-              </PasswordLabel>
-              <InputWrapper>
-                <Input
-                  id="confirmPassword"
-                  name="confirmPassword"
-                  placeholder="Password"
-                  type={isPassword.confirmPassword ? 'text' : 'password'}
-                />
-
-                <EyeBtn
-                  onClick={() => handleTogglePassword('confirmPassword')}
-                  type="button"
-                >
-                  <IconEye width="16" height="16" stroke="#407BFF" fill="none">
-                    <use
-                      xlinkHref={
-                        isPassword.confirmPassword
-                          ? `${sprite}#icon-eye-slash`
-                          : `${sprite}#icon-eye`
-                      }
-                    />
-                  </IconEye>
-                </EyeBtn>
-                <ErrorMessage component="div" name="confirmPassword" />
-              </InputWrapper>
-            </FieldWrapper>
-          </PasswordBox>
-
-          <SubmitBtn type="submit" onClick={handleSubmit}>
-            Save
-          </SubmitBtn>
-        </StyledForm>
-      )}
-    </Formik>
-  );
-};
+//           <SubmitBtn type="submit" onClick={handleSubmit}>
+//             Save
+//           </SubmitBtn>
+//         </StyledForm>
+//       )}
+//     </Formik>
+//   );
+// };
--- src/components/Header/SettingModal/SettingModal.js
+++ src/components/Header/SettingModal/SettingModal.js
@@ -1,5 +1,5 @@
+import { validateUserInfoSchema } from 'schemas/validateUserInfoSchema.js';
 import { useState } from 'react';
-import { SettingForma } from './SettingForma/SettingForma';
 import sprite from '../../../images/sprite.svg';
 import {
   AvatarBox,
@@ -7,17 +7,38 @@ import {
   AvatarBtnText,
   AvatarImg,
   AvatarWrapper,
+  EyeBtn,
+  FieldWrapper,
   FileInput,
+  IconEye,
+  Input,
+  InputWrapper,
+  LabelRadio,
+  PasswordBox,
+  PasswordBoxTitle,
+  PasswordLabel,
+  RadioBox,
+  RadioGroup,
+  StyledForm,
+  SubmitBtn,
   Text,
+  UserLabel,
 } from './SettingModal.styled';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectUser } from 'redux/auth/authSelectors';
 import { updateAvatarThunk } from '../../../redux/auth/authOperations';
+import { ErrorMessage, Formik } from 'formik';
 
 export const SettingModal = ({ isOpen, onRequestClose }) => {
   const { avatarUrl } = useSelector(selectUser);
   const [selectedFile, setSelectedFile] = useState(null);
   const dispatch = useDispatch();
+
+  const [isPassword, setIsPassword] = useState({
+    oldPassword: false,
+    newPassword: false,
+    confirmPassword: false,
+  });
 
   const handleFileChange = event => {
     const file = event.target.files[0];
@@ -33,30 +54,193 @@ export const SettingModal = ({ isOpen, onRequestClose }) => {
     dispatch(updateAvatarThunk(selectedFile));
   };
 
+  const initialValues = {
+    name: '',
+    email: '',
+    gender: 'woman',
+    oldPassword: '',
+    newPassword: '',
+    confirmPassword: '',
+  };
+
+  const handleTogglePassword = passwordKey => {
+    setIsPassword(prev => ({
+      ...prev,
+      [passwordKey]: !prev[passwordKey],
+    }));
+  };
+
+  const handleSubmit = async values => {
+    console.log('data has been saved');
+    onRequestClose();
+  };
+
   return (
-    <>
-      <Text>Your photo</Text>
-      <AvatarWrapper>
-        <AvatarBox>
-          <AvatarImg src={avatarUrl} alt="user avatar" />
-        </AvatarBox>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validateUserInfoSchema}
+      onSubmit={handleSubmit}
+    >
+      {({ errors, touched, values, handleChange }) => (
+        <StyledForm>
+          <Text>Your photo</Text>
+          <AvatarWrapper>
+            <AvatarBox>
+              <AvatarImg src={avatarUrl} alt="user avatar" />
+            </AvatarBox>
 
-        <FileInput
-          name="avatarUrl"
-          type="file"
-          accept="image/*"
-          onChange={handleFileChange}
-          id="uploadInput"
-        />
-        <AvatarBtn type="button" onClick={handleUploadPhoto}>
-          <svg width="16" height="16" stroke="#407BFF" fill="none">
-            <use xlinkHref={`${sprite}#icon-arrow-up-tray`} />
-          </svg>
-          <AvatarBtnText>Upload a photo</AvatarBtnText>
-        </AvatarBtn>
-      </AvatarWrapper>
+            <FileInput
+              name="avatarUrl"
+              type="file"
+              accept="image/*"
+              onChange={handleFileChange}
+              id="uploadInput"
+            />
+            <AvatarBtn type="button" onClick={handleUploadPhoto}>
+              <svg width="16" height="16" stroke="#407BFF" fill="none">
+                <use xlinkHref={`${sprite}#icon-arrow-up-tray`} />
+              </svg>
+              <AvatarBtnText>Upload a photo</AvatarBtnText>
+            </AvatarBtn>
+          </AvatarWrapper>
 
-      <SettingForma onSubmit={() => onRequestClose()} />
-    </>
+          <RadioGroup>
+            <LabelRadio>Your gender identity</LabelRadio>
+            <RadioBox>
+              <Input
+                type="radio"
+                id="woman"
+                name="gender"
+                checked={values.gender === 'woman'}
+                onChange={handleChange}
+                className={errors.gender && touched.gender ? 'error' : ''}
+              />
+              <LabelRadio htmlFor="woman">Girl</LabelRadio>
+              <Input
+                type="radio"
+                id="man"
+                name="gender"
+                checked={values.gender === 'man'}
+                onChange={handleChange}
+              />
+              <LabelRadio htmlFor="man">Man</LabelRadio>
+            </RadioBox>
+          </RadioGroup>
+
+          <FieldWrapper>
+            <UserLabel htmlFor="name">Your name</UserLabel>
+            <InputWrapper>
+              <Input id="name" name="name" />
+              <ErrorMessage component="div" name="name" />
+            </InputWrapper>
+          </FieldWrapper>
+
+          <FieldWrapper>
+            <UserLabel htmlFor="email">Your email</UserLabel>
+            <InputWrapper>
+              <Input id="email" name="email" />
+              <ErrorMessage component="div" name="email" />
+            </InputWrapper>
+          </FieldWrapper>
+
+          <PasswordBox>
+            <PasswordBoxTitle>Password</PasswordBoxTitle>
+
+            <FieldWrapper>
+              <PasswordLabel htmlFor="oldPassword">
+                Outdated password:
+              </PasswordLabel>
+              <InputWrapper>
+                <Input
+                  id="oldPassword"
+                  name="oldPassword"
+                  placeholder="Password"
+                  type={isPassword.oldPassword ? 'text' : 'password'}
+                />
+
+                <EyeBtn
+                  onClick={() => handleTogglePassword('oldPassword')}
+                  type="button"
+                >
+                  <IconEye width="16" height="16" stroke="#407BFF" fill="none">
+                    <use
+                      xlinkHref={
+                        isPassword.oldPassword
+                          ? `${sprite}#icon-eye-slash`
+                          : `${sprite}#icon-eye`
+                      }
+                    />
+                  </IconEye>
+                </EyeBtn>
+                <ErrorMessage component="div" name="oldPassword" />
+              </InputWrapper>
+            </FieldWrapper>
+
+            <FieldWrapper>
+              <PasswordLabel htmlFor="newPassword">New password:</PasswordLabel>
+
+              <InputWrapper>
+                <Input
+                  id="newPassword"
+                  name="newPassword"
+                  placeholder="Password"
+                  type={isPassword.newPassword ? 'text' : 'password'}
+                />
+
+                <EyeBtn
+                  onClick={() => handleTogglePassword('newPassword')}
+                  type="button"
+                >
+                  <IconEye width="16" height="16" stroke="#407BFF" fill="none">
+                    <use
+                      xlinkHref={
+                        isPassword.newPassword
+                          ? `${sprite}#icon-eye-slash`
+                          : `${sprite}#icon-eye`
+                      }
+                    />
+                  </IconEye>
+                </EyeBtn>
+                <ErrorMessage component="div" name="newPassword" />
+              </InputWrapper>
+            </FieldWrapper>
+
+            <FieldWrapper>
+              <PasswordLabel htmlFor="confirmPassword">
+                Repeat new password:
+              </PasswordLabel>
+              <InputWrapper>
+                <Input
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  placeholder="Password"
+                  type={isPassword.confirmPassword ? 'text' : 'password'}
+                />
+
+                <EyeBtn
+                  onClick={() => handleTogglePassword('confirmPassword')}
+                  type="button"
+                >
+                  <IconEye width="16" height="16" stroke="#407BFF" fill="none">
+                    <use
+                      xlinkHref={
+                        isPassword.confirmPassword
+                          ? `${sprite}#icon-eye-slash`
+                          : `${sprite}#icon-eye`
+                      }
+                    />
+                  </IconEye>
+                </EyeBtn>
+                <ErrorMessage component="div" name="confirmPassword" />
+              </InputWrapper>
+            </FieldWrapper>
+          </PasswordBox>
+
+          <SubmitBtn type="submit" onClick={handleSubmit}>
+            Save
+          </SubmitBtn>
+        </StyledForm>
+      )}
+    </Formik>
   );
 };
