493ceb184d111a5ce9a99d05191fee5234f86968
Hennadii Laktionov <morifer13@gmail.com>
2024-01-05 21:31:15 +0200

    delete wrong name and path

--- src/components/ForgotPassword/ForgotPassword.jsx
+++ src/components/ForgotPassword/ForgotPassword.jsx
@@ -2,13 +2,11 @@ import React, { useState } from 'react';
 import { Formik, Form, Field } from 'formik';
 import { SignInLink } from 'components/ForgotPassword/ForgotPassword.styled';
 import { Loader } from 'components/Loader';
-import bottle from '../../images/Desktop/Desktop Bottle for Sign in-1x.png';
 import axios from 'axios';
 import toast, { Toaster } from 'react-hot-toast';
 
 import * as Yup from 'yup';
 import {
-  BottleImage,
   Container,
   Input,
   Label,
@@ -47,7 +45,6 @@ export const ForgotPassword = () => {
   return (
     <>
       <Subcontainer>
-        <BottleImage width="600" src={bottle} />
         <Container>
           <Formik
             initialValues={{ email: '' }}
