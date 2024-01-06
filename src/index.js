e87631d9c795afb2a847dc4a4c4331eb9384f231
NataDent <cherkashyna.n@gmail.com>
2024-01-06 20:20:53 +0200

    Fixed ModalWindow & styles

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/code-jedi-project-03-frontend">
+        <BrowserRouter basename="/OleksiiHvozdukhin/code-jedi-project-03-frontend">
           <ThemeProvider theme={theme}>
             <App />
           </ThemeProvider>
