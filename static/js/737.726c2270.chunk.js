f1d9651d77b94e5db33cf6ceee680d7961df11cb
Alexey Hvozduhin <124602564+OleksiiHvozdukhin@users.noreply.github.com>
2024-01-10 19:30:22 +0200

    Updates

--- static/js/737.726c2270.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcode_jedi_project_03_frontend=self.webpackChunkcode_jedi_project_03_frontend||[]).push([[737],{3737:function(e,n,r){r.r(n),r.d(n,{default:function(){return u}});r(2791);var t=r(184),c=function(){return(0,t.jsx)(t.Fragment,{children:(0,t.jsx)("p",{children:"\u042d\u0442\u043e \u0441\u0442\u0440\u0430\u043d\u0438\u0446\u0430 \u0435\u0440\u0440\u043e\u0440\u0430"})})};function u(){return(0,t.jsx)(c,{})}}}]);
-//# sourceMappingURL=737.726c2270.chunk.js.map
